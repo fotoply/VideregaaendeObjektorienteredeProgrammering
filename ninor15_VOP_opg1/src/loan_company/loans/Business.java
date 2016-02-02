d188aea314a76249a7498531006d8a271acc9541
fotoply <norberg.niels@gmail.com>
2016-02-02 15:20:33 +0100

    Base methods with no function yet

--- ninor15_VOP_opg1/src/loan_company/loans/Business.java
+++ ninor15_VOP_opg1/src/loan_company/loans/Business.java
@@ -5,6 +5,19 @@ package loan_company.loans;
  *
  * @author Niels Norberg
  */
-public class Business {
+public class Business extends Loan {
+    @Override
+    public double getTotal() {
+        return 0;
+    }
 
+    @Override
+    public void setInterestRate() {
+
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0;
+    }
 }
---
+++ ninor15_VOP_opg1/src/loan_company/loans/Loan.java
@@ -0,0 +1,12 @@
+package loan_company.loans;
+
+import loan_company.LoanInterface;
+
+/**
+ * Created 2/2/16
+ *
+ * @author Niels Norberg
+ */
+public abstract class Loan implements LoanInterface {
+    
+}
