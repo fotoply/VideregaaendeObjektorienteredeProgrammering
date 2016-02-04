b6c75e87b026c33d1d94cf166fd9d4d41db6b924
fotoply <norberg.niels@gmail.com>
2016-02-04 11:51:24 +0100

    Fixed missing tab and newline

--- ninor15_VOP_opg1/src/loan_company/model/loans/Loan.java
+++ ninor15_VOP_opg1/src/loan_company/model/loans/Loan.java
@@ -94,12 +94,12 @@ public abstract class Loan implements LoanInterface {
 
     @Override
     public String toString() {
-        return  "\nAccount:\t" + getLoanHolderID() +
+        return  "\nAccount:\t\t" + getLoanHolderID() +
                 "\nName:\t\t" + getLoanHolderName() +
                 "\nDate:\t\t" + getStartedDate().toString() +
                 "\nDuration:\t\t" + getDuration().getTimeInYears() + " years" +
                 "\nAmount:\t\t" + getAmountLoaned() + " " + CURRENCY_DENOTATION +
                 "\nTotal:\t\t" + getTotal() + " " + CURRENCY_DENOTATION +
-                "******************************";
+                "\n******************************\n";
     }
 }
