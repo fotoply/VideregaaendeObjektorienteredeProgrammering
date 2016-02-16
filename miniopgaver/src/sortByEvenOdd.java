45c2cd1db897dbda8fc6065066e95e174ae39486
fotoply <norberg.niels@gmail.com>
2016-02-16 15:00:28 +0100

    Made the method work. Swaps numbers so that the odd's are first in the array and the evens are last. Preserves the ordering of the numbers.

--- miniopgaver/src/sortByEvenOdd.java
+++ miniopgaver/src/sortByEvenOdd.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created 2/16/16
  *
@@ -6,7 +8,26 @@
 public class sortByEvenOdd {
 
     public static void main(String[] args) {
-        //TODO insert init code
+        int[] testData = {71, 1, 2, 68, 36, 59, 70, 22, 81, 89};
+        sortByEvenOdd instance = new sortByEvenOdd();
+        System.out.println(Arrays.toString(instance.evenOdd(testData)));
+    }
+
+    public int[] evenOdd(int [] array) {
+        int minPos = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                swap(array,minPos,i);
+                minPos++;
+            }
+        }
+        return array;
+    }
+
+    public void swap(int[] array, int indexToSwap, int otherIndexToSwap) {
+        int temp1 = array[indexToSwap];
+        array[indexToSwap] = array[otherIndexToSwap];
+        array[otherIndexToSwap] = temp1;
     }
 
 }
