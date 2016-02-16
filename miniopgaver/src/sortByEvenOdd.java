fd7ff1b787c44351d11a9a537a525d31303fe6bc
fotoply <norberg.niels@gmail.com>
2016-02-16 14:41:22 +0100

    Mini assignments for the class

---
+++ miniopgaver/src/sortByEvenOdd.java
@@ -0,0 +1,12 @@
+/**
+ * Created 2/16/16
+ *
+ * @author Niels Norberg
+ */
+public class sortByEvenOdd {
+
+    public static void main(String[] args) {
+        //TODO insert init code
+    }
+
+}
