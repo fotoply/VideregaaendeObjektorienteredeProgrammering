7cd42868df212ff7b694f42811f8af1ecd8e391c
fotoply <norberg.niels@gmail.com>
2016-02-16 15:36:52 +0100

    Refactored to rename the class to have a capital starting letter. Added new main method for testing.

--- miniopgaver/src/SortByEvenOdd.java
+++ miniopgaver/src/SortByEvenOdd.java
@@ -1,16 +1,28 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created 2/16/16
  *
  * @author Niels Norberg
  */
-public class sortByEvenOdd {
+public class SortByEvenOdd {
 
     public static void main(String[] args) {
-        int[] testData = {71, 1, 2, 68, 36, 59, 70, 22, 81, 89};
-        sortByEvenOdd instance = new sortByEvenOdd();
-        System.out.println(Arrays.toString(instance.evenOdd(testData)));
+        /*int[] testData = {71, 1, 2, 68, 36, 59, 70, 22, 81, 89};
+        SortByEvenOdd instance = new SortByEvenOdd();
+        System.out.println(Arrays.toString(instance.evenOdd(testData)));*/
+
+        Random generator = new Random(222);
+        int array[] = new int [10];
+        for(int i = 0; i < array.length; i++){
+            array[i] = generator.nextInt(100);
+        }
+        System.out.println("Input:"+Arrays.toString(array));
+
+        SortByEvenOdd arrMani = new SortByEvenOdd();
+        int result[] = arrMani.evenOdd(array);
+        System.out.println("Output: " +Arrays.toString(result));
     }
 
     public int[] evenOdd(int [] array) {
