ac90e6a7f81e3862ff41983de0fcf9d723dfb914
fotoply <norberg.niels@gmail.com>
2016-03-29 16:30:11 +0200

    Back to array splitting

--- ninor15_VOP_opg2/src/opg4_population/TownStatistics.java
+++ ninor15_VOP_opg2/src/opg4_population/TownStatistics.java
@@ -44,15 +44,8 @@ public class TownStatistics {
         try {
             Scanner scanner = new Scanner(new File(fileName));
             while (scanner.hasNextLine()) {
-                Scanner lineScanner = new Scanner(scanner.nextLine());
-                lineScanner.useDelimiter(";");
-                lineScanner.nextInt();
-                lineScanner.nextInt();
-                DanishTown town = new DanishTown(lineScanner.next(),lineScanner.nextInt(),lineScanner.nextInt());
-                townList.add(town);
-
-                /*String[] values = scanner.nextLine().split(":");
-                townList.add(new DanishTown(values[2],Integer.valueOf(values[3]),Integer.valueOf(values[4])));*/
+                String[] values = scanner.nextLine().split(";");
+                townList.add(new DanishTown(values[2],Integer.valueOf(values[3]),Integer.valueOf(values[4])));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
