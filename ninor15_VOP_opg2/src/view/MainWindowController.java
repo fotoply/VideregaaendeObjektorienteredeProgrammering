b1e95fb8e547f97da456a698d447ca510ac3800c
fotoply <norberg.niels@gmail.com>
2016-03-29 16:53:23 +0200

    Buttons now work

--- ninor15_VOP_opg2/src/view/MainWindowController.java
+++ ninor15_VOP_opg2/src/view/MainWindowController.java
@@ -4,8 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.text.Text;
 import opg2_numberplates.NumberPlates;
+import opg4_population.TownStatistics;
 
 /**
  * Created 3/29/16
@@ -14,6 +16,8 @@ import opg2_numberplates.NumberPlates;
  */
 public class MainWindowController {
     private NumberPlates numberPlates;
+    private TownStatistics townStatistics;
+
     @FXML
     private TextField plateTextField;
 
@@ -25,17 +29,18 @@ public class MainWindowController {
 
     @FXML
     void populationClearClicked(ActionEvent event) {
-
+        populationTextField.clear();
     }
 
     @FXML
     void populationReadFileClicked(ActionEvent event) {
-
+        populationTextField.setText(townStatistics.toString());
     }
 
     @FXML
     void populationSortClicked(ActionEvent event) {
-
+        townStatistics.sort();
+        populationTextField.appendText(townStatistics.toString());
     }
 
     @FXML
@@ -46,6 +51,8 @@ public class MainWindowController {
     @FXML
     void initialize() {
         numberPlates = new NumberPlates();
+        townStatistics = new TownStatistics();
+        townStatistics.readFile("Befolkning.txt");
     }
 
 }
