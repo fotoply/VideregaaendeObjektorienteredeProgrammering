8f9aeb0fa4dfbd838d060b5fc098dcc367bee4b9
fotoply <norberg.niels@gmail.com>
2016-03-29 15:28:17 +0200

    Added naming and new elements and a couple of methods.

--- ninor15_VOP_opg2/src/view/MainWindowController.java
+++ ninor15_VOP_opg2/src/view/MainWindowController.java
@@ -1,10 +1,25 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 /**
  * Created 3/29/16
  *
  * @author Niels Norberg
  */
 public class MainWindowController {
+    @FXML
+    private TextField plateTextField;
+
+    @FXML
+    private Text plateResultText;
+
+    @FXML
+    void plateSearchButtonClicked(ActionEvent event) {
+
+    }
 
 }
