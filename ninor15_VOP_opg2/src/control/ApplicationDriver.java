a5773cf56d4bd2c32799e1bdf5f31fc839f52acf
fotoply <norberg.niels@gmail.com>
2016-03-29 14:46:28 +0200

    Added basic window

---
+++ ninor15_VOP_opg2/src/control/ApplicationDriver.java
@@ -0,0 +1,20 @@
+package control;/**
+ * Created 3/29/16
+ *
+ * @author Niels Norberg
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ApplicationDriver extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ ninor15_VOP_opg2/src/view/MainWindowController.java
@@ -0,0 +1,10 @@
+package view;
+
+/**
+ * Created 3/29/16
+ *
+ * @author Niels Norberg
+ */
+public class MainWindowController {
+
+}
