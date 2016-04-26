8d50efa6fc0ca13fd0aae155d11ede960b2f3bb4
fotoply <norberg.niels@gmail.com>
2016-04-26 13:08:36 +0200

    Added a mainController for launching the program

---
+++ ninor15_VOP_opg3/src/MainController.java
@@ -0,0 +1,27 @@
+/**
+ * Created 4/26/16
+ *
+ * @author Niels Norberg
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("view/MainWindow.fxml"));
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.show();
+    }
+}
