513ddbf321b1f8474ebd94c7e6595ec32d95a76d
fotoply <norberg.niels@gmail.com>
2016-04-26 13:29:40 +0200

    Started implementing ArrayTester

--- ninor15_VOP_opg3/src/arrays/ArrayTester.java
+++ ninor15_VOP_opg3/src/arrays/ArrayTester.java
@@ -1,16 +1,19 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringJoiner;
+
 /**
  * Udleveret klasse erklæring med main() metode til VOP re-eksamen 20. august 2014
  *
  * @author erso
  */
 public class ArrayTester {
-    
-    
 
-
-    
+    private Random generator;
+    private static final int MAX = 100;
+    private int[] intArray;
 
     // Udleveret metode, som bytter om på 2 elementer i arrayet.
     // Bør benyttes ved løsning af opg 3d
@@ -25,9 +28,9 @@ public class ArrayTester {
     // Udleveret main()-metode til test
     // Fjern ud-kommenteringerne efterhånden som opgaven løses.
     public static void main(String[] args) {
-//        int size = 30;
-//        ArrayTester arrayTester = new ArrayTester(size);
-//        System.out.println("constructor: " + arrayTester);
+        int size = 30;
+        ArrayTester arrayTester = new ArrayTester(size);
+        System.out.println("constructor: " + arrayTester);
 
 //        arrayTester.sort();
 //        System.out.println("sorted: " + arrayTester);
@@ -37,4 +40,25 @@ public class ArrayTester {
 
     }
 
+    public ArrayTester(int size) {
+        generator = new Random();
+        intArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            intArray[i] = generator.nextInt(ArrayTester.MAX);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < intArray.length; i++) {
+            if(i%8 == 0) {
+                builder.append("\n");
+            }
+            builder.append(String.format("%02d, ", intArray[i]));
+        }
+        builder.append("\n---------------------\n");
+
+        return builder.toString();
+    }
 }
