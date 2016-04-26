39676b4fe500e8e2e7d0a80bd127961c14070c37
fotoply <norberg.niels@gmail.com>
2016-04-26 13:07:12 +0200

    Added auto updating

--- ninor15_VOP_opg3/src/view/MainWindowController.java
+++ ninor15_VOP_opg3/src/view/MainWindowController.java
@@ -4,8 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import poly_and_strings.FlipUpperLowerManip;
+import poly_and_strings.StringManipulable;
+import poly_and_strings.ToLowerManip;
+import poly_and_strings.ToUpperManip;
 
 public class MainWindowController {
+
+    StringManipulable inputString;
 
     @FXML
     private TextField inputTextField;
@@ -24,5 +30,23 @@ public class MainWindowController {
 
     @FXML
     private RadioButton flipCaseToggle;
+
+    @FXML
+    void initialize() {
+        inputTextField.textProperty().addListener((observable, oldValue, newValue) -> {updateOutput();});
+        caseToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {updateOutput();});
+    }
+
+    public void updateOutput() {
+        if(caseToggleGroup.getSelectedToggle().equals(flipCaseToggle)) {
+            inputString = new FlipUpperLowerManip(inputTextField.getText());
+        } else if(caseToggleGroup.getSelectedToggle().equals(lowerCaseToggle)) {
+            inputString = new ToLowerManip(inputTextField.getText());
+        } else {
+            inputString = new ToUpperManip(inputTextField.getText());
+        }
+
+        outputTextField.setText(inputString.manip());
+    }
 
 }
