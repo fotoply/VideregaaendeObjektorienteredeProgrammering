88deb647300b5af27ba79ba43f14834de8e6a32b
fotoply <fotoply@gmail.com>
2016-04-27 14:39:40 +0200

    Remove boilerplate comments

--- ninor15_VOP_opg3/src/arrays/ArrayTester.java
+++ ninor15_VOP_opg3/src/arrays/ArrayTester.java
@@ -1,31 +1,26 @@
 package arrays;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
- * Udleveret klasse erklæring med main() metode til VOP re-eksamen 20. august 2014
  *
- * @author erso
+ * @author fotoply
  */
 public class ArrayTester {
 
-    private Random generator;
     private static final int MAX = 100;
+    private Random generator;
     private int[] intArray;
 
-    // Udleveret metode, som bytter om på 2 elementer i arrayet.
-    // Bør benyttes ved løsning af opg 3d
-    public void swap(int fromIndex, int toIndex) {
-        int temp = intArray[fromIndex];
-        intArray[fromIndex] = intArray[toIndex];
-        intArray[toIndex] = temp;
+    public ArrayTester(int size) {
+        generator = new Random();
+        intArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            intArray[i] = generator.nextInt(ArrayTester.MAX);
+        }
     }
-    
 
-
-    // Udleveret main()-metode til test
-    // Fjern ud-kommenteringerne efterhånden som opgaven løses.
     public static void main(String[] args) {
         int size = 30;
         ArrayTester arrayTester = new ArrayTester(size);
@@ -36,7 +31,12 @@ public class ArrayTester {
 
         arrayTester.reverse();
         System.out.println("reverse: " + arrayTester);
+    }
 
+    public void swap(int fromIndex, int toIndex) {
+        int temp = intArray[fromIndex];
+        intArray[fromIndex] = intArray[toIndex];
+        intArray[toIndex] = temp;
     }
 
     public void sort() {
@@ -44,16 +44,8 @@ public class ArrayTester {
     }
 
     public void reverse() {
-        for (int i = 0; i < intArray.length/2; i++) {
-            swap(i,intArray.length-1-i);
-        }
-    }
-
-    public ArrayTester(int size) {
-        generator = new Random();
-        intArray = new int[size];
-        for (int i = 0; i < size; i++) {
-            intArray[i] = generator.nextInt(ArrayTester.MAX);
+        for (int i = 0; i < intArray.length / 2; i++) {
+            swap(i, intArray.length - 1 - i);
         }
     }
 
@@ -61,7 +53,7 @@ public class ArrayTester {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < intArray.length; i++) {
-            if(i%8 == 0) {
+            if (i % 8 == 0) {
                 builder.append("\n");
             }
             builder.append(String.format("%02d, ", intArray[i]));
